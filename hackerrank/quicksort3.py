2b025ca95d7cabfb6d32afbf267767ecec9558a4
Jeremiah Marks <Jeremiah@JLMarks.org>
2014-12-28 17:07:31 -0700

    updated quicksort3.  It now works properly for some cases, however larger cases it does not work properly for.

--- hackerrank/quicksort3.py
+++ hackerrank/quicksort3.py
@@ -8,20 +8,27 @@ def swapPositions(ar, pos1, pos2):
 	return ar
 
 
-def quicksortInplace(ar):
+def quicksortInplace(ar,n):
+	retString=""
 	piv=ar[-1]
 	for location in range(len(ar)):
 		if (ar[location]<piv):
-			break
+			pass
 		elif (ar[location]==piv):
 			for x in range(len(ar)):
 				if (ar[x]>piv):
 					ar=swapPositions(ar,x,-1)
+					if (len(ar)==n):
+						retString+= str(ar).replace('[','').replace(']','').replace(',','')+"\n"
 					if (len(ar[:x])>1):
-						ar=quicksortInplace(ar[:x])+ar[x:]
+						ar=quicksortInplace(ar[:x],n)+ar[x:]
+					if (len(ar)==n):
+						retString+= str(ar).replace('[','').replace(']','').replace(',','')+"\n"
 					if (len(ar[x:])>1):
-						ar=ar[:x]+quicksortInplace(ar[x:])
-			print ar
+						ar=ar[:x]+quicksortInplace(ar[x:],n)
+					if (len(ar)==n):
+						retString+= str(ar).replace('[','').replace(']','').replace(',','')+"\n"
+					break
 		else:
 			curloc=location+1
 			placed=False
@@ -32,5 +39,11 @@ def quicksortInplace(ar):
 					ar[location]=curlocval
 					placed=True
 				curloc+=1
-	return ar
+	if (len(ar)==n):
+		print retString
+	else:
+		return ar
 
+m = input()
+ar = [int(i) for i in raw_input().strip().split()]
+quicksortInplace(ar,m)
