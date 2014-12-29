76afddeafc5e9071225e528877a741fabcf886d1
Jeremiah Marks <Jeremiah@JLMarks.org>
2014-12-28 21:19:05 -0700

    updated countingSort4.py  devised a considerably better solution after looking at what others had done, specifically this solution:  https://codepair.hackerrank.com/paper/Am3fVX2r?b=eyJyb2xlIjoiY2FuZGlkYXRlIiwibmFtZSI6ImpsbWFya3MiLCJlbWFpbCI6ImplcmVtaWFoQGpsbWFya3Mub3JnIn0%3D

--- hackerrank/countingSort4.py
+++ hackerrank/countingSort4.py
@@ -2,20 +2,20 @@
 #from https://www.hackerrank.com/challenges/countingsort4
 
 def countingsort(ar):
-	results=[]
-	justNumbers=[]
-	retString=""
-	arrayLength=len(ar)
-	firstHalf=arrayLength/2
-	for x in range(arrayLength):
-		theValue=ar[x]
-		if (x<firstHalf):
-			theValue[1]="-"
-		theValue[0]=int(theValue[0])
-		justNumbers.append(theValue[0])
-	vals=sorted(ar, key=lambda values:values[0])
-	for eachPart in vals:
-		print (eachPart[1]),
+	results=[[] for i in range(100)]
+	for place,value in ar:
+		results[place].append(value)
+	retString= sun(results,[])
+	print ''.join(retString)
+	# for x in range(arrayLength):
+	# 	theValue=ar[x]
+	# 	if (x<firstHalf):
+	# 		theValue[1]="-"
+	# 	theValue[0]=int(theValue[0])
+	# 	justNumbers.append(theValue[0])
+	# vals=sorted(ar, key=lambda values:values[0])
+	# for eachPart in vals:
+	# 	print (eachPart[1]),
 	# for z in range(100):
 	# 	num=(justNumbers.count(z))
 	# 	if (num==0):
@@ -34,6 +34,10 @@ def countingsort(ar):
 m = input()
 ar=[]
 for x in range(m):
-	ar.append((raw_input().split()))
+	t=raw_input().split()
+	if x<m/2:
+		ar.append([int(t[0]), "-"])
+	else:
+		ar.append([int(t[0]), t[1]])
 
 countingsort(ar)
