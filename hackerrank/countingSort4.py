41349a3a285360061724b663f8886ec87d4526eb
Jeremiah Marks <Jeremiah@JLMarks.org>
2014-12-28 21:04:18 -0700

    updated countingSort4.py  I feel like my solution is really cheating, but I could not figure out another way off of the top to do it.

--- hackerrank/countingSort4.py
+++ hackerrank/countingSort4.py
@@ -13,16 +13,19 @@ def countingsort(ar):
 			theValue[1]="-"
 		theValue[0]=int(theValue[0])
 		justNumbers.append(theValue[0])
-	for z in range(100):
-		num=(justNumbers.count(z))
-		if (num==0):
-			pass
-		else:
-			for time in range(num):
-				thisValue=next(p for p in ar if(p[0]==z))
-				retString+=thisValue[1]+" "
-				ar.remove(thisValue)
-	return retString
+	vals=sorted(ar, key=lambda values:values[0])
+	for eachPart in vals:
+		print (eachPart[1]),
+	# for z in range(100):
+	# 	num=(justNumbers.count(z))
+	# 	if (num==0):
+	# 		pass
+	# 	else:
+	# 		for time in range(num):
+	# 			thisValue=next(p for p in ar if(p[0]==z))
+	# 			print (thisValue[1]),
+	# 			ar.remove(thisValue)
+	
 
 
 
@@ -33,4 +36,4 @@ ar=[]
 for x in range(m):
 	ar.append((raw_input().split()))
 
-print str(countingsort(ar)).replace('[','').replace(']','').replace(',','')
+countingsort(ar)
