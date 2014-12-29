93c9fb377acd5bb50ff4176746d062eea1ce4137
Jeremiah Marks <jeremiah@jlmarks.org>
2014-12-29 12:56:28 -0700

    Create iceCreamParlor.py

---
+++ hackerrank/iceCreamParlor.py
@@ -0,0 +1,12 @@
+t = int(input()) # m= number of test cases
+
+for eachCase in range(t):
+	m=int(raw_input()) # m = amount of money to spend
+	n=int(raw_input()) # n = number of flavors
+	ar=map(int, raw_input().strip().split(" "))
+	for f1 in range(len(ar)):
+		for f2 in range(f1+1,len(ar)):
+			if (ar[f1]+ar[f2]==m):
+				print str(f1+1) + " " + str(f2+1)
+
+
