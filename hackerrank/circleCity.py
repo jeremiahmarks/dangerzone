c8e3e3cb993fdb5ac0b60b84336ca8b8d2b74b42
Jeremiah Marks <Jeremiah@JLMarks.org>
2014-12-29 20:38:22 -0700

    added circleCity.py.  It currently passes all test cases except for 2,0 which it says should work, but I know does not.... yeah I forgot to save the stupid file last commit.

--- hackerrank/circleCity.py
+++ hackerrank/circleCity.py
@@ -16,3 +16,18 @@ for x in range(t):
 		print "possible"
 	else:
 		print "impossible"
+
+
+##Testing Function:
+
+def testingFunction(radiusSquared, numberOfStations):
+	placesNeeded=0
+	#radiusSquared,numberOfStations = map(int,raw_input().split())
+	radius = int(radiusSquared**0.5)
+	for x in range(radius):
+		if ((int((radiusSquared-(x**2))**0.5))**2==(radiusSquared-(x**2))):
+			placesNeeded+=4
+	if (placesNeeded<=numberOfStations):
+		print "possible"
+	else:
+		print "impossible"
