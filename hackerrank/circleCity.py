b915b6a9e2478cfc0a25a8af1f421d07a1a9a41d
Jeremiah Marks <Jeremiah@JLMarks.org>
2014-12-29 20:55:33 -0700

    updated circleCity to (hepefully) account for cases where the radius is not a rational number

--- hackerrank/circleCity.py
+++ hackerrank/circleCity.py
@@ -9,6 +9,8 @@ for x in range(t):
 	placesNeeded=0
 	radiusSquared,numberOfStations = map(int,raw_input().split())
 	radius = int(radiusSquared**0.5)
+	if not(radius**2==radiusSquared):
+		radius+=1
 	for x in range(radius):
 		if ((int((radiusSquared-(x**2))**0.5))**2==(radiusSquared-(x**2))):
 			placesNeeded+=4
@@ -24,7 +26,7 @@ def testingFunction(radiusSquared, numberOfStations):
 	placesNeeded=0
 	#radiusSquared,numberOfStations = map(int,raw_input().split())
 	radius = int(radiusSquared**0.5)
-	for x in range(radius):
+	for x in range(radius+1):
 		if ((int((radiusSquared-(x**2))**0.5))**2==(radiusSquared-(x**2))):
 			placesNeeded+=4
 	if (placesNeeded<=numberOfStations):
