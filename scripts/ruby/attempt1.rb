c41e368719c0061db34f08f8e5317cb1d6a43980
Jeremiah Marks <Jeremiah@JLMarks.org>
2015-02-10 00:18:13 -0700

    just updating some work that I completed today.

--- scripts/ruby/attempt1.rb
+++ scripts/ruby/attempt1.rb
@@ -1,8 +1,8 @@
 #!/usr/bin/ruby
 # @Author: jeremiah.marks
 # @Date:   2015-02-09 14:01:01
-# @Last Modified by:   jeremiah.marks
-# @Last Modified time: 2015-02-09 17:56:30
+# @Last Modified 2015-02-10>
+# @Last Modified time: 2015-02-10 00:17:51
 
 
 # A cleaner version of ./attempt0.rb
@@ -32,13 +32,12 @@ end
 
 class Player
 	attr_reader :name, :current_stand
-
 	def add_card(card)
 		#to add a card to their hand.
 		@hand << card
 	end
-
 	def calculate_cards()
+		@current_plan=nil
 		@value_of_hand = [0]
 		@hand.each do |card|
 			if card.point_value == [1,11]
@@ -68,12 +67,10 @@ class Player
 			end #end larger if
 		end #end each do
 	end # end calculate Cards
-
 	def hit_stand_bust
 		# get what the next move should be
 		return @current_plan
 	end #end hitstandbust
-
 	def get_ante(value)
 		if value>@bankroll
 		    response = :OutOfMoney
@@ -83,7 +80,11 @@ class Player
 		end # end if
 		response
 	end # end get_ante
-
+	def ante_check(value)
+		if value<=@bankroll
+			:deal_in
+		end
+	end
 	def display_cards
 		puts "\n\n"
 	  	if @hand.empty?
@@ -99,22 +100,30 @@ class Player
 	  	end # end if
 	  	print "\n"
 	end # end display cards
-
+	def cards_as_array
+		temp_holder = []
+		@hand.each do |card|
+			temp_holder << card.get_values
+		temp_holder
+	end
 	def game_end
 		@hand[0].flip
 		self.display_cards
-		@hand=[]
 	end # end game_end
-
 	def accept_payment(amount)
+		#
 		@bankroll+=amount
 	end # end acceptPayment
-
+	def clear_hands
+		#making sure that hands are empty when starting a new hand
+		@hand=[]
+	end
 	def initialize(name, cutoff)
 		@name=name
 		@hand=[]
 		@bankroll=1000
 		@cutoff_point=cutoff
+		@current_plan=:hit
 	end #end init
 end #end player
 
@@ -143,32 +152,140 @@ class Game_of_blackjack
 
       	end
     end
-
-    def deal_a_card_up
-    	@people.each do |person|
+    def deal_a_card_up(player=nil)
+    	if player
     		a=@deck.delete @deck.sample
     		a.flip
-    		person.add_card(a)
+    		player.add_card(a)
+    	else
+	    	@people.each do |person|
+    			a=@deck.delete @deck.sample
+    			a.flip
+    			person.add_card(a)
+    		end
     	end
     end
-
-    def deal_a_card_down
-    	@people.each do |person|
+    def deal_a_card_down(player=nil)
+    	if player
     		a=@deck.delete @deck.sample
-    		person.add_card(a)
+    		player.add_card(a)
+    	else
+	    	@people.each do |person|
+    			a=@deck.delete @deck.sample
+    			person.add_card(a)
+    		end
     	end
     end
-
     def get_players_cards
     	@people.each do |person|
     		person.display_cards
     	end
     end
+    def award_winner(winner, winnings=@current_pot)
+    	winner.accept_payment(winnings)
+    	puts "\n\n\n\tWinner!\n#{winner.name} won #{winnings.to_s}\n"
+    	@current_pot = @current_pot - winnings
+    end
+    def need_to_deal?
+    	needs_cards=0
+    	@players.each { |player| player.hit_stand_bust == :hit ? needs_cards +=1 }
+    	needs_cards > 0
+    end
+    def determine_winner
+    	##This probably took up most of the area of the paper I outlined. 
+    	##Things that will be good to have to figure this out:
+    	## 		array with players planned moves
+    	## 		array with players cards and current plan and current stand
+    	plans={}
+    	stand_values={}
+    	full_cards={}
+    	counter=0
+    	@players.each do |player|
+    		player.game_end
+    		plans[counter]= player.hit_stand_bust
+    		stand_values[counter] = player.current_stand
+    		full_cards[counter] = player.cards_as_array
+    	end
+    	if plans.has_value? :win
+    		if plans.values.count(:win)==1
+    			#there is only one winner
+    			award_winner(@players[plans.key(:win)])
+    		else
+    			total_wins=plans.values.count(:win)
+    			takings = @current_pot / total_wins
+    			@players.each {|player| player.hit_stand_bust== :win ? self.award_winner(player, takings) : nil }
+    		end
+    	elsif plans.has_value? :stand
+    		total_stands=plans.values.count(:stand)
+    		if total_stands == 1
+    			award_winner(@players[plans.key(:stand)])
+    		else
+    			#figure out who is closest
+    			
+    			
 
+
+
+
+
+
+    end
+    def one_hand
+    	#Make sure that the pot is empty
+    	@current_pot=0
+    	#and that the sentinel variable stands ready
+    	@hand_winner=nil
+    	#get a deck of cards
+    	self.new_deck
+    	#make sure that the players are not trying to slide an extra card in
+    	@people.each do |player|
+    		player.clear_hands
+    		#now their hands are clear, lets collect their ante and deal them in
+    		if player.ante_check  #If a player can ante in, check if they are added to the table already. 
+    			unless @players.include? player# If they are not, in the table
+    				self.add_to_table(player) #invite them
+    			end
+    			@current_pot+=player.get_ante(@ante) # take their money
+    			self.deal_a_card_down(player) # and give them a card down
+    		else #basically if they did not pass the ante check
+    			if @players.include? player #if they are at the table, ask them to leave
+    				self.remove_from_table(player)
+    			end
+    		end
+    	end
+    	if @players.count == 0 
+    		@game_winner = :No_one
+    	elsif @players.count == 1
+    		@game_winner = @players[0]
+    		self.award_winner(@game_winner)
+    	else
+    		while self.need_to_deal?
+    			@players.each do |player|
+    				if player.hit_stand_bust == :hit
+    					deal_a_card_up(player)
+    					player.calculate_cards
+    				end
+    			end
+    		end
+
+
+
+    			
+
+
+    end
+    def add_to_table(player)
+    	@players << player
+    end
+    def remove_from_table(player)
+    	@players.delete(player)
+    end
   	def initialize(*players)
+  		@ante=1
   		@game_winner=nil
   		@round_winner=nil
-  		@people=[]
+  		@people=[] #this includes people who have gone broke
+  		@players=[] #This is people who are still at the table
   		players.each do |player|
   			@people << Player.new(player.to_s, 17)
   		end
---
+++ scripts/ruby/tg6.rb
@@ -0,0 +1,15 @@
+#!/usr/bin/ruby
+# @Author: Jeremiah Marks
+# @Date:   2015-02-09 21:48:23
+# @Last Modified 2015-02-09
+# @Last Modified time: 2015-02-09 21:49:38
+
+def k
+	ba=nil
+
+	if ba
+		puts "ba"
+	else
+		puts "not ba"
+	end
+end
