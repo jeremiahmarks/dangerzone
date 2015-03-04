9ee04af0d589822b1e50ed9520d54fb7874cdf6d
Jeremiah Marks <Jeremiah@JLMarks.org>
2015-03-04 10:21:27 -0700

    Just an update of some files that somehow have been created since the last push.

---
+++ scripts/bash/ipaddress.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+# @Author: Jeremiah Marks
+# @Date:   2015-03-04 02:24:35
+# @Last Modified by:   Jeremiah Marks
+# @Last Modified time: 2015-03-04 02:26:55
+
+dig +short myip.opendns.com @resolver1.opendns.com
