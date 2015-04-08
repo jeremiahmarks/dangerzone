172bfb6cdd0acaca91f1b04d9fab17e5b8476fb0
Jeremiah Marks <Jeremiah@JLMarks.org>
2015-04-07 19:42:25 -0700

    redirected error messages to dev null

--- scripts/ruby/git_update_assistant/e1.rb
+++ scripts/ruby/git_update_assistant/e1.rb
@@ -1,8 +1,8 @@
 #!/usr/bin/ruby
 # @Author: Jeremiah Marks
 # @Date:   2015-03-16 22:50:24
-# @Last Modified 2015-04-03
-# @Last Modified time: 2015-04-03 16:59:05
+# @Last Modified 2015-04-07
+# @Last Modified time: 2015-04-07 19:39:32
 
 
 gitsFinder = %x( find ~/ -name ".git" 2>/dev/null )
@@ -15,7 +15,7 @@ gits=gitsFinder.split("\n")
 
 
 gits.each do |changeDir|
-    cmd = "cd " + changeDir + " && pwd"
+    cmd = "cd " + changeDir + " 2>/dev/null && pwd"
     puts %x[ #{cmd} ]
 end
 
