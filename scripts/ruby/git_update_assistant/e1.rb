fda7f3d16c0b9606e18def91e757118aabf68886
Jeremiah Marks <Jeremiah@JLMarks.org>
2015-04-07 22:51:52 -0700

    added output of git status in each directory

--- scripts/ruby/git_update_assistant/e1.rb
+++ scripts/ruby/git_update_assistant/e1.rb
@@ -2,7 +2,7 @@
 # @Author: Jeremiah Marks
 # @Date:   2015-03-16 22:50:24
 # @Last Modified 2015-04-07
-# @Last Modified time: 2015-04-07 20:02:09
+# @Last Modified time: 2015-04-07 20:04:50
 
 
 gitsFinder = %x( find ~ ! -path '*/share/Trash/*'  -name ".git"  2>/dev/null )
@@ -17,5 +17,7 @@ gits=gitsFinder.split("\n")
 gits.each do |changeDir|
     cmd = "cd " + changeDir + " 2>/dev/null && pwd"
     puts %x[ #{cmd} ]
+    checkStatus = "git status"
+    puts %x[ #{checkStatus} ]
 end
 
